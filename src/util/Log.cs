13bf49378ee6f57fb4742b1f45a3ccd45c7406d0
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-09-14 08:43:29 -0400

    Added AssemblyVersion.tt  Added InstallChecker  Changed default log level for  release to ERROR  Added AsemblyFileVersion

--- src/util/Log.cs
+++ src/util/Log.cs
@@ -3,44 +3,47 @@ using UnityEngine;
 
 namespace Nereid
 {
-   namespace SAVE
-   {
-      public static class Log
-      {
-         public enum LEVEL { OFF=0, ERROR=1, WARNING=2, INFO=3, DETAIL=4, TRACE=5 };
+    namespace SAVE
+    {
+        public static class Log
+        {
+            public enum LEVEL { OFF = 0, ERROR = 1, WARNING = 2, INFO = 3, DETAIL = 4, TRACE = 5 };
 
-         public  static LEVEL level = LEVEL.INFO ;
+#if DEBUG
+            public static LEVEL level = LEVEL.INFO;
+#else
+            public  static LEVEL level = LEVEL.ERROR;
+#endif
+            private static readonly String PREFIX = "S.A.V.E: ";
 
-         private static readonly String PREFIX = "S.A.V.E: ";
+            public static LEVEL GetLevel()
+            {
+                return level;
+            }
 
-         public static LEVEL GetLevel()
-         {
-            return level;
-         }
+            public static void SetLevel(LEVEL level)
+            {
+                Debug.Log("log level " + level);
+                Log.level = level;
+            }
 
-         public static void SetLevel(LEVEL level)
-         {
-            Debug.Log("log level "+level);
-            Log.level = level;
-         }
+            public static LEVEL GetLogLevel()
+            {
+                return level;
+            }
 
-         public static LEVEL GetLogLevel()
-         {
-            return level;
-         }
+            private static bool IsLevel(LEVEL level)
+            {
+                return level == Log.level;
+            }
 
-         private static bool IsLevel(LEVEL level)
-         {
-            return level == Log.level;
-         }
+            public static bool IsLogable(LEVEL level)
+            {
+                return level <= Log.level;
+            }
 
-         public static bool IsLogable(LEVEL level)
-         {
-            return level <= Log.level;
-         }
-
-         private static void LogOnMainThread(String msg)
-         {
+            private static void LogOnMainThread(String msg)
+            {
 #if false
                 if(SAVE.configuration.asynchronous)
             {
@@ -48,41 +51,41 @@ namespace Nereid
             }
             else
 #endif
-            {
-               Debug.Log(msg);
+                {
+                    Debug.Log(msg);
+                }
             }
-         }
 
-         public static void Trace(String msg)
-         {
-            if (IsLogable(LEVEL.TRACE))
+            public static void Trace(String msg)
             {
-               LogOnMainThread(PREFIX + msg);
+                if (IsLogable(LEVEL.TRACE))
+                {
+                    LogOnMainThread(PREFIX + msg);
+                }
             }
-         }
 
-         public static void Detail(String msg)
-         {
-            if (IsLogable(LEVEL.DETAIL))
+            public static void Detail(String msg)
             {
-               LogOnMainThread(PREFIX + msg);
+                if (IsLogable(LEVEL.DETAIL))
+                {
+                    LogOnMainThread(PREFIX + msg);
+                }
             }
-         }
 
 
-         public static void Info(String msg)
-         {
-            if (IsLogable(LEVEL.INFO))
+            public static void Info(String msg)
             {
-               LogOnMainThread(PREFIX + msg);
+                if (IsLogable(LEVEL.INFO))
+                {
+                    LogOnMainThread(PREFIX + msg);
+                }
             }
-         }
 
-         // for debuging only; calls should be removed for release
-         public static void Test(String msg)
-         {
-            //if (IsLogable(LEVEL.INFO))
+            // for debuging only; calls should be removed for release
+            public static void Test(String msg)
             {
+                //if (IsLogable(LEVEL.INFO))
+                {
 #if false
                     if (SAVE.configuration.asynchronous)
                {
@@ -90,16 +93,16 @@ namespace Nereid
                }
                else
 #endif
-               {
-                  Debug.LogWarning(PREFIX + "TEST:" + msg);
-               }
+                    {
+                        Debug.LogWarning(PREFIX + "TEST:" + msg);
+                    }
+                }
             }
-         }
 
-         public static void Warning(String msg)
-         {
-            if (IsLogable(LEVEL.WARNING))
+            public static void Warning(String msg)
             {
+                if (IsLogable(LEVEL.WARNING))
+                {
 #if false
                     if (SAVE.configuration.asynchronous)
                {
@@ -107,16 +110,16 @@ namespace Nereid
                }
                else
 #endif
-               {
-                  Debug.LogWarning(PREFIX + msg); ;
-               }               
+                    {
+                        Debug.LogWarning(PREFIX + msg); ;
+                    }
+                }
             }
-         }
 
-         public static void Error(String msg)
-         {
-            if (IsLogable(LEVEL.ERROR))
+            public static void Error(String msg)
             {
+                if (IsLogable(LEVEL.ERROR))
+                {
 #if false
                     if (SAVE.configuration.asynchronous)
                {
@@ -124,17 +127,17 @@ namespace Nereid
                }
                else
 #endif
-               {
-                  Debug.LogError(PREFIX + msg);
-               }
+                    {
+                        Debug.LogError(PREFIX + msg);
+                    }
+                }
             }
-         }
 
-         public static void Exception(Exception e)
-         {
-            String msg = PREFIX + "exception caught: " + e.GetType() + ": " + e.Message;
-            if (IsLogable(LEVEL.ERROR))
+            public static void Exception(Exception e)
             {
+                String msg = PREFIX + "exception caught: " + e.GetType() + ": " + e.Message;
+                if (IsLogable(LEVEL.ERROR))
+                {
 #if false
                     if (SAVE.configuration.asynchronous)
                {
@@ -142,12 +145,12 @@ namespace Nereid
                }
                else
 #endif
-               {
-                  Log.Error(msg);
-               }
+                    {
+                        Log.Error(msg);
+                    }
+                }
             }
-         }
 
-      }
-   }
+        }
+    }
 }
